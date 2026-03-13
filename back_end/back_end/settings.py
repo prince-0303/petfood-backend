598b29c95fafb5c23edd2110425bd8f5ec732b8f
prince-0303 <princebinil2003@gmail.com>
2026-03-13 12:54:00 +0530

    configure settings for render deployment

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -9,7 +9,11 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    config('RENDER_HOST', default=''),
+]
 
 
 # Application definition
